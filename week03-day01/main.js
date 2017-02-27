6ccba6fadeff978836c355ecbacae414ed36f675
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-02-27 14:45:29 +0000

    playing iwth javascript

---
+++ week03-day01/main.js
@@ -0,0 +1,28 @@
+console.log('in main.js')
+
+//shorthand to create objects
+var o = {};
+var s = '';
+var b = true;
+var n = 1;
+var a = [];
+console.log('Playing with different types:');
+var numberAsString = '5';
+var number1 = 3;
+var number2 = 7;
+console.log(numberAsString + number1);
+console.log(typeof (numberAsString + number1));
+console.log(number1 + numberAsString);
+console.log(typeof (number1 + numberAsString));
+console.log(number1 + number2);
+console.log('Harry' + 'Potter');
+console.log('Harry' - 'Potter');
+console.log(parseInt(numberAsString));
+console.log(parseInt(numberAsString) + number1 + number2);
+console.log(parseInt(3.1415927));
+console.log(parseInt('3.1415927'));
+console.log(parseInt(3.56));
+console.log(parseFloat(3.1415927));
+console.log(parseFloat('3.1415927'));
+console.log(parseFloat(3.56));
+console.log(parseFloat('blah'));
