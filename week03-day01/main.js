14fb218b182f3ca12a16d173e0b7260e416fea5d
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-02-28 10:41:47 +0000

    day one notes

--- week03-day01/main.js
+++ week03-day01/main.js
@@ -1,12 +1,17 @@
-console.log('in main.js')
-
-//shorthand to create objects
+console.log('in main.js');
 var o = {};
 var s = '';
 var b = true;
 var n = 1;
 var a = [];
-console.log('Playing with different types:');
+console.log('--- Using "typeof" operator:');
+console.log(typeof o);
+console.log(typeof s);
+console.log(typeof b);
+console.log(typeof n);
+console.log(typeof a);
+
+console.log('--- Playing with different types:');
 var numberAsString = '5';
 var number1 = 3;
 var number2 = 7;
@@ -27,72 +32,79 @@ console.log(parseFloat('3.1415927'));
 console.log(parseFloat(3.56));
 console.log(parseFloat('blah'));
 
+console.log('--- Control flow:');
 var moneyInPocket = 20;
-if(moneyInPocket > 10){
-  console.log('Another drink please!')
-} else if (moneyInPocket >= 5 ){
-  console.log('Make mine a half then')
+
+if (moneyInPocket > 10) {
+  console.log('Another drink please!');
+} else if (moneyInPocket > 5) {
+  console.log('Make mine a half then');
 } else {
   console.log('Time to go home');
 }
 
-var countryOfOrigin = 'France';
+var countryOfOrigin = 'Kenya';
 var greeting = '';
-switch(countryOfOrigin){
+
+switch(countryOfOrigin) {
   case 'France':
     greeting = 'Bonjour';
     break;
   case 'Spain':
-    greeting = 'Hola';
+    greeting = '¡Hola!';
     break;
-  case 'Indonesia';
+  case 'Indonesia':
     greeting = 'Selamat sore';
     break;
-    var countryOfOrigin = 'Kenya';
-  var greeting = '';
+  case 'Kenya':
+    greeting = 'Jambo!';
+    break;
+  default:
+    greeting = 'Well hello there';
+    break;
+}
+console.log(greeting);
 
-  switch(countryOfOrigin) {
-    case 'France':
-      greeting = 'Bonjour';
-      break;
-    case 'Spain':
-      greeting = '¡Hola!';
-      break;
-    case 'Indonesia':
-      greeting = 'Selamat sore';
-      break;
-    case 'Kenya':
-      greeting = 'Jambo!';
-      break;
-    default:
-      greeting = 'Well hello there';
-      break;
-  }
-  console.log(greeting);
+var dayOfWeek = 'Thursday';
+// logical AND
+if (dayOfWeek.toUpperCase() === 'THURSDAY' || dayOfWeek.toUpperCase() === 'FRIDAY') {
+  console.log('Pub thisarvo!');
+} else {
+  console.log('Bummer :-(');
+}
 
-  var dayOfWeek = 'Thursday';
-  // logical AND
-  if (dayOfWeek.toUpperCase() === 'THURSDAY' || dayOfWeek.toUpperCase() === 'FRIDAY') {
-    console.log('Pub thisarvo!');
+console.log('--- truthy/falsey:');
+var truthyThings = [1, 0, 'something', '', -1, {}, [], null, undefined, NaN, 'false', '0'];
+
+for(var i = 0 ; i < truthyThings.length ; i++) {
+  if (truthyThings[i]) {
+    console.log('  ', truthyThings[i], '--> truthy');
   } else {
-    console.log('Bummer :-(');
+    console.log('  ', truthyThings[i], '--> falsey');
   }
+}
 
-  console.log('--- truthy/falsey:');
-  var truthyThings = [1, 0, 'something', '', -1, {}, [], null, undefined, NaN, 'false', '0'];
+var yourName = prompt('What is your name?');
 
-  for(var i = 0 ; i < truthyThings.length ; i++) {
-    if (truthyThings[i]) {
-      console.log('  ', truthyThings[i], '--> truthy');
-    } else {
-      console.log('  ', truthyThings[i], '--> falsey');
-    }
-  }
+if (yourName) {
+  alert('Nice one, ' + yourName);
+} else {
+  alert('Not sure what your name is!');
+}
 
-  var yourName = prompt('What is your name?');
+console.log('--- Loops:');
+var question = 'Now using a while() loop. What is your name?';
+yourName = prompt(question);
+while (!yourName) {
+  alert('Not sure what your name is!');
+  yourName = prompt(question);
+}
+alert('Nice one, ' + yourName);
 
-  if (yourName) {
-    alert('Nice one, ' + yourName);
-  } else {
+do {
+  yourName = prompt('Now using a do/while() loop. What is your name?');
+  if (!yourName) {
     alert('Not sure what your name is!');
   }
+} while (!yourName);
+alert('Nice one, ' + yourName);
