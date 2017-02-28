96700b60d070da50e64b00aa071b6ebba8bcb07b
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-02-28 16:46:03 +0000

    more obj oriented JS. use of instances and consructors.

--- week03-day02/main.js
+++ week03-day02/main.js
@@ -48,6 +48,9 @@ and so on and so forth
 /*ARRAY
 var ages = [ 24, 23, 29, 32];*/
 
+var capitalCity = 'London'; //global variable
+
+
 //FUNCTIONS
 console.log('----- FUNCTIONS ----')
 //when this function is called you get these parameters
@@ -57,14 +60,24 @@ function createPerson(firstNameParam, lastNameParam, emailParam, ageParam){
     firstName: firstNameParam,
     lastName: lastNameParam,
     email: emailParam,
-    age: ageParam
-
+    age: ageParam,
+    capitalCity: capitalCity,
+    fullName: function() {
+      return firstNameParam + ' ' + lastNameParam + ' from ' + capitalCity;
+    }
   };
+
   return newPerson; //this function makes the result of this code available outside the function
 }
 
 var harald = createPerson('Harald', 'Kumar', 'h.kumar@example.com', 15);
-console.log('newPerson:', harald);
+console.log('newPerson:', harald.fullName());
+var tola = createPerson('Tola', 'Olaoke', 'tolaoke@spartaglobal.co', 21);
+var asma = createPerson('Asma', 'Chaima', 'achaima@spartaglobal.co', 21);
+people = [harald, tola, asma];
+people.push(harald);
+people.push(tola);
+people.push(asma);
 
 function isOldEnough(age){
   return (age >= 18);
@@ -76,4 +89,55 @@ if (isOldEnough(17)){
    {
      console.log('Come back when you are older.')
    }
+}
+
+// var oldEnoughComment;
+// for (i = 0; i < people.length; i++){
+//   oldEnoughComment = (isOldEnough(people[i].age))?
+//   'is old enough':
+//   'is NOT old enough';
+//
+//   console.log(people[i].fullName(), oldEnoughComment);
+  //ternary operator                                           if                else
+//   console.log(people[i].fullName(), isOldEnough(people[i].age))?'is old enough':'is NOT old enough');
+// }
+
+console.log('------ Obj Oriented Javascript:')
+               //class
+var coin = new Circle(1.2);
+var plate = new Circle(7);
+var circles = [coin, plate];
+
+//one way to write an instance method
+//constructor function -- can me a brand new one of these. "new Circle"
+function Circle(radius) {
+       //uppercase for a CONSTANT var
+  //this.PI = 3.14
+  this.radius = radius;
+  this.circumference = function(){
+    return 2 * Circle.PI * this.radius;
+  };
+  this.area = function () {
+    return Circle.PI * this.radius * this.radius;
+  };
+}
+
+//static (or class) property/var
+Circle.PI = 22/7;
+
+//another instance method 
+//for every new instance of this circle i want you to create a new area for it
+//attached to the prototype -- it is part of the circle class. when javascript does new:
+//it says in this obj that is passed back into the var here
+//in that obj there going to be a method called area
+Circle.prototype.area = function() {
+
+  return Circle.PI * this.radius * this.radius;
+};
+
+for (i = 0; i < circles.length; i++){
+                         //property
+console.log('Radius is', circles[i].radius);
+console.log('circumference is', circles[i].circumference());
+console.log('Area is:', circles[i].area()); //method
 }
